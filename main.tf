ba4085d0c45e1944a9016a3b3fda26e74645b86e
armzig <bellotaophyc@outlook.com>
2023-02-16 19:41:26 +0100

    12 02 To enable remore state storage with amazon s3

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "works_state" {
+    bucket = "works-up-and-running-state"
+
+    # Prevent accidental deletion of this S3 bucket
+    lifecycle {
+        prevent_destroy = true
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "works_state" {
+  bucket = aws_s3_bucket.works-up-and-running-state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "works_state" {
+  name           = "works-up-and-running-state"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
